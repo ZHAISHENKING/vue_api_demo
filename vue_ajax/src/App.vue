b6e6698c3103efc00026f9884fa80afd9c59062a
ZHAISHENKING <18700790825@163.com>
2019-10-03 15:41:49 +0800

    vue_resource使用

--- vue_ajax/src/App.vue
+++ vue_ajax/src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p v-if="!repoName">loading...</p>
+    <p v-else>最受欢迎的库是<a :href="repoUrl">{{repoName}}</a></p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      repoUrl: "",
+      repoName: ""
+    }
+  },
+  
+  mounted() {
+    const url = `https://api.github.com/search/repositories?q=vu&sort=starts`
+    this.$http.get(url).then(
+      res => {
+        const result = res.data
+        const mostRepo = result.items[0]
+        this.repoUrl = mostRepo.html_url
+        this.repoName = mostRepo.name
+
+      },
+    )
+  },
 }
 </script>
 
