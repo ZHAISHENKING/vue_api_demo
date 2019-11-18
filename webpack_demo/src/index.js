ae744214da6d786f68bb74a053a18fe48a662a7d
ZHAISHENKING <18700790825@163.com>
2019-11-18 10:03:56 +0800

    开发工具

--- webpack_demo/src/index.js
+++ webpack_demo/src/index.js
@@ -1,19 +1,17 @@
 import _ from 'lodash'
-import './style.css'
-import myImage from './my-image.png'
+import printMe from './print'
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button')
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    // 在项目中引用新创建的style.css
-    element.classList.add('hello')
+    
+    btn.innerHTML = '点我触发打印'
+    btn.onclick = printMe;
 
-    // 将图像插入到div
-    let myIcon = new Image();
-    myIcon.src = myImage;
-    element.appendChild(myIcon)
+    element.appendChild(btn);
     return element;
   }
   
--- webpack_demo/webpack.config.js
+++ webpack_demo/webpack.config.js
@@ -1,18 +1,89 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist')
+//     },
+//     module: {
+//         rules: [
+//             /** 
+//              * webpack 根据正则表达式，来确定应该查找哪些文件
+//              * ，并将其提供给指定的 loader。在这种情况下
+//              * 以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader
+//              * */
+//             {
+//                 test: /\.css$/,
+//                 use: [
+//                     'style-loader',
+//                     'css-loader'
+//                 ]
+//             },
+//             /** 
+//              * 使用file-loader，轻松将图片内容混合到CSS中
+//             */
+//             {
+//                 test: /\.(png|svg|jpg|gif)$/,
+//                 use: [
+//                     'file-loader'
+//                 ]
+//             },
+//             /** 
+//              * file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录
+//             */
+//             {
+//                 test: /\.(woff|woff2|eot|ttf|otf)$/,
+//                 use: [
+//                     'file-loader'
+//                 ]
+//             },
+//             /**
+//              * JSON文件的支持是内置的
+//              * 对csv和xml数据的支持需安装 csv-loader和xml-loader
+//              */
+//             {
+//                 test: /\.(csv|tsv)$/,
+//                 use: ['csv-loader']
+//             },
+//             {
+//                 test: /\.xml$/,
+//                 use: ['xml-loader']
+//             }
+//         ]
+//     }
+// }
 
 module.exports = {
-    entry: './src/index.js',
+    /** 
+     * 我们将在 entry 添加 src/print.js 作为新的入口起点（print），
+     * 然后修改 output，以便根据入口起点名称动态生成 bundle 名称
+    */
+   entry: {
+       app: './src/index.js',
+       print: './src/print.js'
+   },
+   /** 开发工具，追踪源代码错误位置 */
+   devtool: 'inline-source-map',
+   /** 
+    * 使用HtmlWebpackPlugin解决
+    * 入口文件名修改，index.html仍引用原入口文件打包的文件的问题
+    * 执行打包会重新生成index.html，并引入依赖模块
+   */
+   plugins: [
+       new CleanWebpackPlugin(),
+       new HtmlWebpackPlugin({
+           title: 'Output Management'
+       })
+   ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
-            /** 
-             * webpack 根据正则表达式，来确定应该查找哪些文件
-             * ，并将其提供给指定的 loader。在这种情况下
-             * 以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader
-             * */
             {
                 test: /\.css$/,
                 use: [
@@ -20,35 +91,11 @@ module.exports = {
                     'css-loader'
                 ]
             },
-            /** 
-             * 使用file-loader，轻松将图片内容混合到CSS中
-            */
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
                     'file-loader'
                 ]
-            },
-            /** 
-             * file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录
-            */
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            /**
-             * JSON文件的支持是内置的
-             * 对csv和xml数据的支持需安装 csv-loader和xml-loader
-             */
-            {
-                test: /\.(csv|tsv)$/,
-                use: ['csv-loader']
-            },
-            {
-                test: /\.xml$/,
-                use: ['xml-loader']
             }
         ]
     }
